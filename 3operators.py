54d62e6c23afcf58cdc9471b7bd86ed586748a57
ayushi08081996 <08.96ayushi@gmail.com>
2024-11-15 14:07:47 +0000

    Add files via upload

---
+++ 3operators.py
@@ -0,0 +1,12 @@
+#3operators.py
+
+# 3.1 Operators
+
+# 3.1.1 Multiply 4 with 8, and print the result.
+# TODO: Write your code below
+
+print(4 * 8)
+
+# 3.1.2 Divide 30 by 6, and print the result.
+# TODO: Write your code below
+print(30 / 6)
